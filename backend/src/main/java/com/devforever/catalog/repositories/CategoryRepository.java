b61b3d002cad876bb438f6bda612dceaf5dfcb85
victor moraes <victorhardflipmoraes@outlook.com>
2022-12-16 16:46:51 -0400

    CategoryDTO

--- backend/src/main/java/com/devforever/catalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/devforever/catalog/repositories/CategoryRepository.java
@@ -1,4 +1,4 @@
-package com.devforever.catalog;
+package com.devforever.catalog.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- backend/src/main/java/com/devforever/catalog/services/CategoryService.java
+++ backend/src/main/java/com/devforever/catalog/services/CategoryService.java
@@ -1,13 +1,15 @@
 package com.devforever.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devforever.catalog.CategoryRepository;
+import com.devforever.catalog.dto.CategoryDTO;
 import com.devforever.catalog.entities.Category;
+import com.devforever.catalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
@@ -17,7 +19,8 @@ public class CategoryService {
 	
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {		
+		List<Category> list = repository.findAll();		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
